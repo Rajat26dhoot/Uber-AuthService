edb762eee0c434959c7c76d148413315200cd253
Rajatdhoot26 <2rajatd6@gmail.com>
2025-01-27 04:28:42 +0530

    First commit

---
+++ src/main/java/com/example/uberauthenticservice/UberAuthenticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberauthenticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthenticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberAuthenticServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uberauthenticservice/UberAuthenticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberauthenticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAuthenticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
